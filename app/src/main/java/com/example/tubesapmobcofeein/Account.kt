b2dbefab24cf3e34aa6ae136d7e0c18e98fb3dbd
adinenayla <naylaadine@gmail.com>
2021-06-18 23:57:15 +0700

    udah sampe gatau lupa

--- app/src/main/java/com/example/tubesapmobcofeein/Account.kt
+++ app/src/main/java/com/example/tubesapmobcofeein/Account.kt
@@ -3,16 +3,16 @@ package com.example.tubesapmobcofeein
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.widget.Button
 
 class Account : AppCompatActivity() {
-    private lateinit var btnep : TextView
+    private lateinit var btnedit : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.frontsettings)
-        btnep = findViewById(R.id.editprofile)
-        btnep.setOnClickListener(
+        setContentView(R.layout.account)
+        btnedit = findViewById(R.id.editprofile)
+        btnedit.setOnClickListener {
             startActivity(Intent(this,Editaccount::class.java))
-        )
+        }
     }
 }
--- app/src/main/java/com/example/tubesapmobcofeein/Editaccount.kt
+++ app/src/main/java/com/example/tubesapmobcofeein/Editaccount.kt
@@ -2,15 +2,12 @@ package com.example.tubesapmobcofeein
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
 
 class Editaccount : AppCompatActivity() {
     private lateinit var db: FirebaseFirestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.editprofile)
+        setContentView(R.layout.editaccount)
     }
 }
